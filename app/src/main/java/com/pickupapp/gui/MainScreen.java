1babea7dc7fd7b88946d0df4aebf0356f8149681
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-09-24 23:39:10 -0300

    splash screen

--- app/src/main/java/com/pickupapp/gui/MainScreen.java
+++ app/src/main/java/com/pickupapp/gui/MainScreen.java
@@ -1,14 +1,16 @@
-package com.pickupapp;
+package com.pickupapp.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.pickupapp.R;
+
+public class MainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_screen);
     }
 }
