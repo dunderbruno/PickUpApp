4427e8fe59174c1c4a5a87dd59003e8f801dbd1a
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-09-29 20:11:19 -0300

    update cadastro usuario; adicionado o cadastro da pessoa
    atualização do cadastro de usuario para adicionar o cadastro da pessoa relacionada a esse usuario, já realizando a conexão; utilizado metodo para verificação de quando for finalizado a execução da chamada para a api.

--- app/src/main/java/com/pickupapp/persistencia/PersonDAO.java
+++ app/src/main/java/com/pickupapp/persistencia/PersonDAO.java
@@ -1,5 +1,9 @@
 package com.pickupapp.persistencia;
 
+import android.content.Context;
+import android.util.Base64;
+import android.util.Log;
+
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -7,52 +11,44 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.pickupapp.dominio.User;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import android.content.Context;
-import android.util.Log;
 
-import com.pickupapp.dominio.User;
-
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import android.util.Base64;
-//import com.pickupapp.infra.Session;
-
-public class UserDAO {
+public class PersonDAO {
     private Context context;
     private String host = "https://pickupbsiapi.herokuapp.com";
 
-    public UserDAO(Context contexto){
+    public PersonDAO(Context contexto){
         this.context = contexto;
     }
 
-    public User register(final User user) throws JSONException{
-        user.setId(0);
-        String url = host + "/user";
+    public User criarPessoa(final User user) throws JSONException {
+        user.getPerson().setId(0);
+        String url = host + "/person";
         JSONObject postparams = new JSONObject();
-        final String[] idusuario = {""};
-        postparams.put("username", user.getUsername());
-        postparams.put("password", user.getPassword());
+        postparams.put("name", user.getUsername());
+        postparams.put("surname", user.getPassword());
         final AtomicInteger requestsCounter = new AtomicInteger(0);
         final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postparams, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    Log.d("aqui", String.valueOf(response.get("new_user_id").toString()));
-                    user.setId(Integer.parseInt(response.get("new_user_id").toString()));
+                    user.getPerson().setId(Integer.parseInt(response.get("person_id").toString()));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d("resposta", String.valueOf(error));
+                Log.d("resposta criarpessoa", String.valueOf(error));
             }
         }){
             @Override
@@ -74,12 +70,9 @@ public class UserDAO {
             public void onRequestFinished(Request<Object> request) {
                 requestsCounter.decrementAndGet();
                 if (requestsCounter.get() == 0) {
-                    Log.d("aqui", String.valueOf(user.getId()));
-                    if (user.getId() != 0) {
-                        Log.d("aqui", "chegou2");
-                        PersonDAO pessoa = new PersonDAO(context);
+                    if (user.getPerson().getId() != 0) {
                         try {
-                            pessoa.criarPessoa(user);
+                            setPessoaUsuario(user);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -90,4 +83,34 @@ public class UserDAO {
         return user;
     }
 
+    private void setPessoaUsuario(final User user) throws JSONException {
+        String url = host + "/user/"+ String.valueOf(user.getId())+ "/person";
+        JSONObject postparams = new JSONObject();
+        postparams.put("person_id", user.getPerson().getId());
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postparams, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("resposta setpessoa", String.valueOf(response));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("resposta setpessoa", String.valueOf(error));
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                String credentials = user.getUsername()+":"+user.getPassword();
+                String auth = "Basic "
+                        + Base64.encodeToString(credentials.getBytes(),
+                        Base64.NO_WRAP);
+                params.put("Authorization", auth);
+                params.put("x-access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.PODXncdn8smjXC-GZfhaMXIJ9M4fYAvwfZUT9xNgO3Y");
+                return params;
+            }};
+        RequestQueue requestQueue = Volley.newRequestQueue(this.context);
+        requestQueue.add(jsonObjectRequest);
+    }
 }
