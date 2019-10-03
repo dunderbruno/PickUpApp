487175836acc1180e603a3be42ba8d76531704ae
Unknown <djair.batista@outlook.com>
2019-10-03 14:36:14 -0300

    Menu Lateral Padrão Configurado
    necessário condiguração

--- app/src/main/java/com/pickupapp/gui/Splash.java
+++ app/src/main/java/com/pickupapp/gui/Splash.java
@@ -18,7 +18,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(Splash.this, MainScreen.class);
+                Intent i = new Intent(Splash.this, DrawerActivity.class);
                 startActivity(i);
                 finish();
             }
