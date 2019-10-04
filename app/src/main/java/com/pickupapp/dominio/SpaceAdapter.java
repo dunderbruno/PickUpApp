9ce0e7483a125fb3f6048deb33231679e4ab4cc3
Unknown <djair.batista@outlook.com>
2019-10-03 23:20:18 -0300

    listagem espaços

---
+++ app/src/main/java/com/pickupapp/dominio/SpaceAdapter.java
@@ -0,0 +1,41 @@
+package com.pickupapp.dominio;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.pickupapp.R;
+
+import java.net.ConnectException;
+import java.util.ArrayList;
+
+public class SpaceAdapter extends ArrayAdapter<Space> {
+
+    private final Context context;
+    private final ArrayList<Space> espacos;
+
+    public SpaceAdapter(Context context, ArrayList<Space> espacos){
+        super(context, R.layout.linha_spaces, espacos);
+        this.context = context;
+        this.espacos = espacos;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.linha_spaces, parent, false);
+
+        TextView nameSpace = rowView.findViewById(R.id.id_nome);
+        TextView phoneSpace = rowView.findViewById(R.id.id_phone);
+
+        nameSpace.setText(espacos.get(position).getName());
+        phoneSpace.setText(espacos.get(position).getPhone());
+
+        return rowView;
+
+    }
+
+
+}
--- app/src/main/java/com/pickupapp/gui/fragments/ListSpacesFragment.java
+++ app/src/main/java/com/pickupapp/gui/fragments/ListSpacesFragment.java
@@ -8,8 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.pickupapp.R;
+import com.pickupapp.dominio.Space;
+import com.pickupapp.dominio.SpaceAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +32,39 @@ public class ListSpacesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+        //ListView lista = (ListView) getView().findViewById(R.id.lista_spaces_fragment);
+        ///ArrayAdapter<Space> adapter = new SpaceAdapter(getActivity(), retornarSpaces());
+        ///lista.setAdapter(adapter);
+
+
         return inflater.inflate(R.layout.fragment_list_spaces, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        ListView lista = (ListView) getView().findViewById(R.id.lista_spaces_fragment);
+        ArrayAdapter<Space> adapter = new SpaceAdapter(getActivity(), retornarSpaces());
+        lista.setAdapter(adapter);
+    }
+
+    private ArrayList<Space> retornarSpaces(){
+        ArrayList<Space> espacos = new ArrayList<Space>();
+        Space espaco = new Space();
+        espaco.setName("Espaco 1");
+        espaco.setPhone("12345");
+        espacos.add(espaco);
+        espaco.setName("Espaco 2");
+        espaco.setPhone("56789");
+        espacos.add(espaco);
+
+        return espacos;
+
+
+
     }
 
 }
