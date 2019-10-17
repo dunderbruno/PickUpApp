61a517916f7b67877b38d0f55cdef8fbccd5a05a
Unknown <djair.batista@outlook.com>
2019-10-17 03:32:43 -0300

    Tela Usuário Bugada

--- app/src/main/java/com/pickupapp/gui/fragments/UserProfileFragment.java
+++ app/src/main/java/com/pickupapp/gui/fragments/UserProfileFragment.java
@@ -8,7 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.mikhaellopez.circularimageview.CircularImageView;
 import com.pickupapp.R;
 
 /**
@@ -16,17 +21,55 @@ import com.pickupapp.R;
  */
 public class UserProfileFragment extends Fragment {
 
+    private CircularImageView imagemUsuario;
+    private TextView nomeUsuario;
+    private TextView emailUsuario;
+    private TextView enderecoUsuario;
+    private TextView telefoneUsuario;
+    private Button botaoEditarPerfil;
 
-    public UserProfileFragment() {
-        // Required empty public constructor
-    }
+
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View inflate = inflater.inflate(R.layout.fragment_profile, container, false);
+        botaoEditarPerfil = inflate.findViewById(R.id.botaoEditarPerfilId);
+        botaoEditarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Chamar Activity Editar
+                Toast.makeText(getContext(),"activity editar", Toast.LENGTH_LONG);
+            }
+        });
+
+
+        imagemUsuario = inflate.findViewById(R.id.circularImageId);
+        nomeUsuario = inflate.findViewById(R.id.nomeUsuarioId);
+        emailUsuario = inflate.findViewById(R.id.emailUsuarioId);
+        enderecoUsuario = inflate.findViewById(R.id.enderecoUsuarioId);
+        telefoneUsuario= inflate.findViewById(R.id.telefoneUsuarioId);
+        setUsuario();
+
+
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+
+
+
+
+
     }
+
+    private void setUsuario() {
+        //retornar objeto do banco
+        //setar imagem, nome, email, endereco e usuario pelo banco
+    }
+
 
 }
